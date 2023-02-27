2c78434c6c88715481eb70b14f3dc36e09e9f59e
Toby Flanagan <tobyf@DESKTOP-883F8OA.localdomain>
2023-02-27 23:33:05 +0000

    comments

--- Lexer Files/lexer.c
+++ Lexer Files/lexer.c
@@ -11,7 +11,7 @@ I confirm that I will not publish the program online or share it with anyone wit
 Student Name: Tobias Samuel Flanagan
 Student ID: 201510161
 Email: sc21tf@leeds.ac.uk
-Date Work Commenced: 22/02/2023
+Date Work Commenced: 22/02/20j23
 *************************************************************************/
 
 
@@ -24,9 +24,17 @@ Date Work Commenced: 22/02/2023
 
 // YOU CAN ADD YOUR OWN FUNCTIONS, DECLARATIONS AND VARIABLES HERE
 
+#define eof -1
 
 
+//global array for all key words
+const char* keywords[21] = {"class", "constructor", "method", "function"
+  "int", "boolean", "char", "void", "var", "static", "field", "let", "do"
+  "if", "else", "while", "return", "true", "false", "null", "this"     };
 
+
+//global file pointer
+FILE* CurrentFile;
 
 
 // IMPLEMENT THE FOLLOWING functions
@@ -39,16 +47,91 @@ Date Work Commenced: 22/02/2023
 // if everything goes well the function should return 1
 int InitLexer (char* file_name)
 {
-  return 0;
+
+  //ninimal error checking currently
+  CurrentFile = fopen(file_name, "r");
+
+  //check if file exists
+  if (CurrentFile == NULL){
+
+    printf("ERROR: Can not find file: '%s'\n", file_name);
+    return 0;
+
+  }
+
+  //TODO: check if file is a JACK file
+
+
+  return 1;
 }
 
 
 // Get the next token from the source file
 Token GetNextToken ()
 {
-	Token t;
-  t.tp = ERR;
-  return t;
+
+  //initialise token
+	Token token;
+  token.tp = ERR;
+
+  //c must be int to capture EOF correctly
+  int c = getc(CurrentFile);
+
+  //skip through whitespace
+  while(isspace(c)){
+
+    c = getc(CurrentFile);
+
+  }
+
+
+  //check for comments
+  if(c == '/'){
+
+    //printf("read first slash\n");
+    c = getc(CurrentFile);
+    if(c == '/'){
+
+      //skip all writing until end of line or EOF
+      printf("line is a comment\n");
+
+      while(c != '\n' || c != eof){
+        c = getc(CurrentFile);
+      }
+
+    }
+    else if(c == '*'){
+
+
+      //skip all writing until closing brace or eof
+      printf("line is a comment until closing\n");
+
+      while(c != eof){
+        if(c == '*'){
+          c = getc(CurrentFile);
+          if(c == '/'){
+            //end of comment
+            break;
+          }
+        }
+      }
+
+    }
+    else{
+
+      printf("line is not a comment\n");
+      ungetc(c, CurrentFile);
+
+    }
+  }
+
+
+
+
+
+
+
+  return token;
 }
 
 // peek (look) at the next token in the source file without removing it from the stream
@@ -71,6 +154,15 @@ int main ()
 {
 	// implement your main function here
   // NOTE: the autograder will not use your main function
+
+  InitLexer("Ball.jack");
+
+  Token t = GetNextToken();
+  while (t.tp != eof){
+
+    t = GetNextToken();
+
+  }
 
   
 	return 0;
